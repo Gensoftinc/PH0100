db30501402563af23159c8be3add87d9d67bc13d
Anand Golechha <Anand Golechha@192.168.1.128>
2018-06-30 11:57:19 -0700

    PH0100 : Production house initial implementation.

---
+++ PH0100/src/main/java/com/gensoft/Ph0100Application.java
@@ -0,0 +1,12 @@
+package com.gensoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ph0100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ph0100Application.class, args);
+	}
+}
